a002ddd80159b864a346fc6b336c4e4b64d0daaf
JackXu <xgz.cz@foxmail.com>
2023-07-03 11:39:25 +0800

    Add HttpClient proxy setting

--- src/main/java/de/rieckpil/courses/config/WebClientConfig.java
+++ src/main/java/de/rieckpil/courses/config/WebClientConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.ProxyProvider;
+
+import java.net.InetSocketAddress;
 
 @Configuration
 public class WebClientConfig {
@@ -21,6 +24,7 @@ public class WebClientConfig {
     HttpClient httpClient =
         HttpClient.create()
             .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2_000)
+            .proxy(proxy -> proxy.type(ProxyProvider.Proxy.HTTP).address(new InetSocketAddress("127.0.0.1", 7890)))
             .doOnConnected(
                 connection ->
                     connection
